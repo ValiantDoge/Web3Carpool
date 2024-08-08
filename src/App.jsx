bdc9bc68ccaf2775ba3ad9fda9f451efbb4228d3
roygitstenhub <108938077+roygitstenhub@users.noreply.github.com>
2024-08-09 01:00:50 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,70 @@
-import { useState } from 'react'
-import carLogo from '../public/car-sharing.png'
-import './App.css'
+import { createBrowserRouter, RouterProvider, Route, Link, Outlet } from "react-router-dom"
+import Home from "./components/Home"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import Profile from "./components/Profile"
+import Dreiver from "./components/Dreiver"
+import Passenger from "./components/Passenger"
+import PublishRide from "./components/PublishRide"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const Layout = () => {
+    return (
+      <div className=" md:w-9/12 mx-auto ">
+        <Outlet />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      errorElement:<Error/>,
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/login',
+          element: <SignIn />
+        },
+        {
+          path: '/register',
+          element: <SignUp />
+        },
+        {
+          path: '/profile/:id',
+          element: <Profile />
+        },
+        {
+          path: '/logout',
+          element: <SignIn />
+        },
+        {
+          path:'/dreiver',
+          element:<Dreiver/>
+        },
+        {
+          path:'/passenger',
+          element:<Passenger/>
+        },
+        {
+          path:'/publishride',
+          element:<PublishRide/>
+        }
+  
+      ]
+    }
+  ])
 
   return (
     <>
-      <div>
-        <a href="#" target="_blank">
-          <img src={carLogo} className="logo" alt="Car logo" />
-        </a>
-        
-      </div>
-      <h1>Carpooling application on the Blockchain</h1>
-      <div className="card">
-        <h1 className="text-3xl font-bold underline text-red-700">
-          Hello world! Carpool
-        </h1>
-       
-      </div>
-     
+        <RouterProvider router={router} >
+        </RouterProvider>
     </>
   )
 }
---
+++ src/components/Dreiver.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { BsArrowUpRight } from 'react-icons/bs';
+
+const Dreiver = () => {
+    return (
+        <div className='w-full flex flex-col  min-h-screen '>
+            <div className='w-full p-2 flex justify-between items-center '>
+                <h1 className=' capitalize text-[2rem] p-2 font-bold text-blue-500'>carPooling - Dreiver Panel </h1>
+
+                <div className=' flex gap-6 p-2'>
+                    <button className='px-4 py-2 bg-blue-500 text-white rounded-md text-[14px] '><Link to={''}>Switch Account</Link></button>
+                    <button className='px-4 py-2 bg-blue-500 text-white rounded-md text-[14px]'><Link to={''}>Logout</Link></button>
+                </div>
+            </div>
+
+            <div className='grid grid-cols-10  mt-6 '>
+                <div className=' p-2 m-4 mt-4 bg-blue-50 rounded-md shadow-md  md:col-span-6 glass'>
+                    <h2 className=' capitalize text-[2rem] p-2 font-semibold'>Welcome User </h2>
+                    <div className=' p-2 flex items-center'>
+                        <span className='font-bold'>Account ID</span> : djjjdjjdjjdjjdjjjdjjdjjjdjjdjjjdjdj
+                    </div>
+
+                    <div className=' p-2 flex items-center'>
+                        <span className='font-bold'>Account Balance</span> : 78.80080808800808008
+                    </div>
+                </div>
+
+                <div className=' p-2 m-4 mt-4 bg-indigo-50 shadow-md rounded-md md:col-span-4 glass '>
+                    <h2 className=' capitalize text-[2rem] p-2 font-semibold'>Menu Options </h2>
+                    <div className='p-2 flex flex-col gap-3'>
+                        <button className='px-4 py-2 bg-blue-500 text-white rounded-md text-[14px] '><Link>
+                            Booked Ride
+                            <BsArrowUpRight className='text-white inline ml-2 ' />
+                        </Link></button>
+
+                        <button className='px-4 py-2 bg-blue-500 text-white rounded-md text-[14px] '><Link>
+                            View All Ride
+                            <BsArrowUpRight className='text-white inline ml-2 ' />
+                        </Link></button>
+
+                        <button className='px-4 py-2 bg-blue-500 text-white rounded-md text-[14px]  '><Link>
+                            Search By Location
+                            <BsArrowUpRight className='text-white inline ml-2 ' />
+                        </Link></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dreiver
