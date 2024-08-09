ba19269cfd66ce718ede379cd502a7abfda5bc61
ValiantDoge <agnelofernandes475@gmail.com>
2024-08-09 10:35:58 +0530

    Revert to d7e428b

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,10 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{html,js}",
-    "./components/**/*.{html,js}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
 
