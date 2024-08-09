67b627d3840a43259266a9fddb3238c64edd6e03
ValiantDoge <agnelofernandes475@gmail.com>
2024-08-09 11:43:43 +0530

    Revert to d7e428b again....

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,28 @@
-import { createBrowserRouter, RouterProvider, Route, Link, Outlet } from "react-router-dom"
-import Home from "./components/Home"
-import SignIn from "./components/SignIn"
-import SignUp from "./components/SignUp"
-import Profile from "./components/Profile"
-import Dreiver from "./components/Dreiver"
-import Passenger from "./components/Passenger"
-import PublishRide from "./components/PublishRide"
+import { useState } from 'react'
+import carLogo from '../public/car-sharing.png'
+import './App.css'
 
 function App() {
-
-
-  const Layout = () => {
-    return (
-      <div className=" md:w-9/12 mx-auto "> 
-        {/* thsi is outlet  */}
-        <Outlet />
-      </div>
-    )
-  }
-
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      errorElement:<Error/>,
-      element: <Layout />,
-      children: [
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/login',
-          element: <SignIn />
-        },
-        {
-          path: '/register',
-          element: <SignUp />
-        },
-        {
-          path: '/profile/:id',
-          element: <Profile />
-        },
-        {
-          path: '/logout',
-          element: <SignIn />
-        },
-        {
-          path:'/dreiver',
-          element:<Dreiver/>
-        },
-        {
-          path:'/passenger',
-          element:<Passenger/>
-        },
-        {
-          path:'/publishride',
-          element:<PublishRide/>
-        }
-  
-      ]
-    }
-  ])
+  const [count, setCount] = useState(0)
 
   return (
     <>
-        <RouterProvider router={router} >
-        </RouterProvider>
+      <div>
+        <a href="#" target="_blank">
+          <img src={carLogo} className="logo" alt="Car logo" />
+        </a>
+        
+      </div>
+      <h1>Carpooling application on the Blockchain</h1>
+      <div className="card">
+        <h1 className="text-3xl font-bold underline text-red-700">
+          Hello world!
+        </h1>
+       
+      </div>
+     
     </>
-  );
+  )
 }
 
 export default App
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/socket.io/map': {
-        target: 'http://localhost:3000/map',
-        changeOrigin: true,
-        ws: true
-      }
-    }
-  },
-  
   plugins: [react()],
 })
