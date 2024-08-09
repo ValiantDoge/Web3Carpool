8f3be21c6ad0d062f4a472944770297fb841dd52
ValiantDoge <agnelofernandes475@gmail.com>
2024-08-09 12:08:22 +0530

    Revert to no no no

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        ws: true
-      }
-    }
-  },
-  
   plugins: [react()],
 })
